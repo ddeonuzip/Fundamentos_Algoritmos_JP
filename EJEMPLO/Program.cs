61250bed1db700d438a70822f68e2c5ac19737e7
Ddeonuu <227641181+ddeonuzip@users.noreply.github.com>
2025-10-01 11:08:10 -0600

    SEM07

---
+++ EJEMPLO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJEMPLO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad, contador, acumulador = 0;
+            double promedio;
+
+            for (contador = 1; contador < 26; contador++)
+            {
+                Console.WriteLine("Ingrese su edad: ");
+                edad = int.Parse(Console.ReadLine());
+                contador = contador + 1;
+                acumulador = acumulador + edad;
+
+            }
+            promedio = acumulador / 26;
+                Console.WriteLine("La edad promedio es: " + promedio);
+          
+
+        }
+    }
+}
