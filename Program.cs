a1430863560c34fdb461c2958e2508eb97c8a4f5
Ddeonuu <227641181+ddeonuzip@users.noreply.github.com>
2025-11-19 12:14:56 -0500

    Add files via upload
    SEM14_JP

--- Program.cs
+++ Program.cs
@@ -1,44 +1,89 @@
 ﻿using System;
 
 
-namespace proy1
+namespace SEM14_JP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-
-            //ARREGLOS BIDIMENSIONAL
-
-
-
-            //DECFLARAMOS NUESTRO ARREGLO BIDIMENSIONAL
-            //tres columnas y diez filas
-            string[,] arregloEstudiante1 = new string[10, 3];
-
-            //DECLARAMOS EL ARREGLO BIDIMENSIONAL Y SUS VALORES
-
-
-            string[,] arregloEstudiante2 = { { "EST01", "PEPITO", "ISC" }, { "EST02","JUANITA","NUT" }, { "EST03", "LUCHITO", "ISC" },{ "EST04", "LEO", "PSIC" }, { "EST05", "FLORCITA", "PSIC" } };
-
-            //MOSTRAMOS E PANTALLA EL INDEX F1, INDEX C1
-            //Console.WriteLine("fila 1/columna 1: " + arregloEstudiante2[1, 1]);
-
-            Console.WriteLine("CÓDIGO   ESTUDIANTE  CARRERA");
-
-            //RRECOREMOS TODO EL ARREGLO BIDIMENSIONAL
-            for (int i = 0; i<arregloEstudiante2.GetLength(0); i++)
+            string nom1, nom2;
+            while (true)
             {
-                Console.Write("\nfila " + i);
+                Console.Write("Ingrese el primer nombre: ");
+                nom1 = Console.ReadLine().Trim();
+                if (!string.IsNullOrWhiteSpace(nom1)) break;
+                else Console.WriteLine("No se permiten espacios en blanco.\n");
 
-                for (int j = 0; j < arregloEstudiante2.GetLength(1); j++)
-                {
-                    Console.Write(" "+arregloEstudiante2[1,j]+" ");
-                }
-                //HACEMOS UN SLATO DE LINEA
-                Console.WriteLine();   
+
             }
 
+            while (true)
+            {
+                Console.Write("Ingrese el segundo nombre: ");
+                nom2 = Console.ReadLine().Trim();
+                if (!string.IsNullOrWhiteSpace(nom2)) break;
+                else Console.WriteLine("No se permiten espacios en blanco.\n");
+
+
+            }
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Cantidad de cáracter nombre1: " + nom1.Length);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Cantidad de cáracter nombre2: " + nom2.Length);
+            Console.ResetColor();
+            Console.WriteLine("Nombre1 en mayús: " + nom1.ToUpper());
+            Console.WriteLine("Nombre2 en minús: " + nom2.ToLower());
+
+
+
+            if (nom1.CompareTo(nom2) == 0) Console.WriteLine("Nombres son iguales");
+            else Console.WriteLine("Nombres no son iguales");
+
+            if (nom1.Contains(nom2)) Console.WriteLine("Si existe 'an'");
+            else Console.WriteLine("No existe 'an'");
+
+            if (nom1.IndexOf("a") != -1) Console.WriteLine("La primera posición de 'a' es " + nom1.IndexOf("a"));
+            else Console.WriteLine("No existe 'a'");
+
+            if (nom2.LastIndexOf("a") != -1) Console.WriteLine("La última posición de 'a' es " + nom2.LastIndexOf("a"));
+            else Console.WriteLine("No existe 'a'");
+
+
+            Console.WriteLine("Insertando UPN al inicio: " + nom1.Insert(0, "UPN"));
+            Console.WriteLine("Insertando SISTEMAS a lo último: " + nom2.Insert(nom2.Length,"SISTEMAS"));
+
+            if (nom2.Length > 5) Console.WriteLine("Eliminando desde la quinta prosición: " + nom2.Remove(5));
+            else Console.WriteLine("No hay quinta posición");
+
+            if (nom1.Contains("a")) Console.WriteLine("Reemplazando la 'a' con '@': " + nom1.Replace("a", "@"));
+            else Console.WriteLine("No existe 'a'");
+
+            if (nom1.Contains("a"))
+            {
+                string[] partes = nom1.Split('a');
+                for (int i = 0; i < partes.Length; i++)
+                    Console.Write(partes[i] + " ->");
+            }
+            else Console.WriteLine("No existe 'a'");
+
+            if (nom2.Length > 4) Console.WriteLine("Subcadena: " + nom2.Substring(4));
+            else Console.WriteLine("No tiene almenos 4 carácteres");
+
+            char[] nom=nom1.ToCharArray();
+            for (int i = 0; i < nom.Length; i++)
+                Console.Write(nom[i]);
+
+            for (int i=nom.Length-1;i>=0; i--)
+                Console.Write(nom[i]);
+
+            Array.Sort(nom);
+            string ordenado=new string(nom);
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nOrdenado: " + ordenado);
+            Console.ResetColor();
+            Console.ReadKey();
 
         }
     }
